29c7882f247cb6488fbbc36844c12a2af6953f1d
Rene Schwertfeger <rene.schwertfeger1998@gmail.com>
2023-11-16 15:42:05 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.2' 
 DESCRIPTION = 'Simple database connector for python'
 LONG_DESCRIPTION = 'Python module for simple connection and editing of databases.'
 
@@ -14,10 +14,10 @@ setup(
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=["mysql_connector_repackaged"], # add any additional packages that 
+        install_requires=["mysql-connector"], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
-        keywords=['python', 'database', 'connector', 'simple', 'mysql', 'mariadb', 'sqlite', 'sql', 'connector', 'db', 'database-connector', 'database-connection', 'database-connection-python', 'database-connector-python', 'database-connection-mysql', 'database-connector-mysql', 'database-connection-sqlite', 'database-connector-sqlite', 'database-connection-mariadb', 'database-connector-mariadb', 'database-connection-sql', 'database-connector-sql'],
+        keywords=['python', 'database', 'connector', 'simple', 'mysql', 'mariadb', 'sqlite', 'sql', 'connector', 'db', 'database-connector'],
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Developers",
