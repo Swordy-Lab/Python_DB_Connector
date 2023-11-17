8d55ea279af5dbf8073c912eb7a8895346748b0d
Rene Schwertfeger <rene.schwertfeger1998@gmail.com>
2023-11-17 01:20:08 +0100

    0.0.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = '0.0.8' 
+VERSION = '0.0.9' 
 DESCRIPTION = 'Simple database connector for python'
 this_directory = Path(__file__).parent
 LONG_DESCRIPTION = (this_directory / "README.md").read_text()
